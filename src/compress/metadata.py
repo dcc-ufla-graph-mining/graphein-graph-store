fafc9562ad516cf7caaa550a8b52b7702caf3a1c
heliohsilva <helio.medeiros_silva@outlook.com>
2025-09-10 13:15:53 -0300

    teste

--- src/compress/metadata.py
+++ src/compress/metadata.py
@@ -740,7 +740,7 @@ def main():
             pdb_codes.append(line.strip().upper())
 
     for i in range(len(pdb_codes)):
-        edge_construction_functions = [v for k, v in edge_imutable_func_attributes.items()]
+        edge_construction_functions = [add_aromatic_interactions, add_aromatic_sulphur_interactions, add_backbone_carbonyl_carbonyl_interactions]
 
         params_to_change_list.append({"granularity": "CA", 
                                     "edge_construction_functions": edge_construction_functions})
