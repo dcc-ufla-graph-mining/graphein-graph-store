c8bb91cae03abcae5cc69e3e123b13d505225dfd
Helio Henrique Medeiros Silva <helio.medeiros_silva@outlook.com>
2026-06-02 17:25:10 -0300

    melhoria no arquivo run_metadata_with_different_datasets

--- run_metadata_with_different_datasets.sh
+++ run_metadata_with_different_datasets.sh
@@ -30,23 +30,21 @@ python -m memory_profiler src/compress/trash_measure_memory.py > "times/trash.tx
 
 EXCOUNT=0
 
-# for dataset in data/*.txt
-# do
-    # for i in {1..5};
-    # do
-        # dataset_name=$(basename $dataset)
-        # export DATASET=$dataset_name
-        dataset_name="bcl_ppigremlin.txt"
+for dataset in data/*.txt
+do
+    for i in {1..5};
+    do
+        dataset_name=$(basename $dataset)
         export DATASET=$dataset_name
         export EXCOUNT
         echo $dataset_name
         python src/compress/main.py >> /app/logs/metadata.log 2>&1
-        # mprof run --python python <script>
-        mprof run --python python src/compress/measure_memory_v1.py > "times/${dataset_name}_v1.txt"
-        mprof plot --output memory_usage_v1.pdf
-        mprof run --python python src/compress/measure_memory_v2.py > "times/${dataset_name}_v2.txt"
-        mprof plot --output memory_usage_v2.pdf
+
+        # mprof run --python python src/compress/measure_memory_v1.py > "times/${dataset_name}_v1.txt"
+        # mprof plot --output memory_usage_v1.pdf
+        # mprof run --python python src/compress/measure_memory_v2.py > "times/${dataset_name}_v2.txt"
+        # mprof plot --output memory_usage_v2.pdf
 
         EXCOUNT=$i
-    # done
-# done
+    done
+done
