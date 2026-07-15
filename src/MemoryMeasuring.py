d965e329f5ae27bb4b73e550b4b8affeec9a9c26
Helio Henrique Medeiros Silva <helio.medeiros_silva@outlook.com>
2026-07-14 21:42:17 -0300

    correções na classe MemoryMeasuring para aderir à modificaçao metodológica

--- src/MemoryMeasuring.py
+++ src/MemoryMeasuring.py
@@ -37,9 +37,6 @@ class MemoryMeasuring:
     def pdb_id_to_edges_serialized_memory(self):
         return len(pk.dumps(self.__body_parts["pdb_id_to_edges"]))/1024/1024
 
-    def node_global_attr_keys_memory(self):
-        return asizeof.asizeof(self.__body_parts["node_global_attr_keys"])/1024/1024
-
     def node_local_attr_keys_memory(self):
         return asizeof.asizeof(self.__body_parts["node_local_attr_keys"])/1024/1024
 
@@ -48,14 +45,12 @@ class MemoryMeasuring:
 
     def attr_keys_memory(self):
         return (
-            self.node_global_attr_keys_memory() +
             self.node_local_attr_keys_memory() +
             self.edge_attr_keys_memory()
         )
     
     def attr_keys_serialized_memory(self):
         return (
-            len(pk.dumps(self.__body_parts["node_global_attr_keys"]))/1024/1024 +
             len(pk.dumps(self.__body_parts["node_local_attr_keys"]))/1024/1024 +
             len(pk.dumps(self.__body_parts["edge_attr_keys"]))/1024/1024
             )
@@ -72,23 +67,10 @@ class MemoryMeasuring:
     def node_attr_values_serialized_memory(self):
         return len(pk.dumps(self.__body_parts["node_attr_values"]))/1024/1024
 
-    def node_global_attr_keyvalue_mapping_memory(self):
-        vetor = self.__body_parts["node_global_attr_keyvalue_mapping"]
-
-        return asizeof.asizeof(vetor)/1024/1024
-    
-    def node_global_attr_keyvalue_mapping_serialized_memory(self):
-        return len(pk.dumps(self.__body_parts["node_global_attr_keyvalue_mapping"]))/1024/1024
-
     def node_local_attr_keyvalue_mapping_memory(self):
         vetor = self.__body_parts["node_local_attr_keyvalue_mapping"]
 
         return asizeof.asizeof(vetor)/1024/1024
-    
-    def teste(self):
-        vetor = self.__body_parts["node_global_attr_keyvalue_mapping"]
-
-        return vetor.nbytes/1024/1024
     
     def node_local_attr_keyvalue_mapping_serialized_memory(self):
         return len(pk.dumps(self.__body_parts["node_local_attr_keyvalue_mapping"]))/1024/1024
@@ -119,7 +101,6 @@ class MemoryMeasuring:
 
     def node_attributes_memory(self):
         return (
-        self.node_global_attr_keyvalue_mapping_memory() +
         self.node_local_attr_keyvalue_mapping_memory()
         )
 
@@ -141,7 +122,6 @@ class MemoryMeasuring:
             self.node_attr_values_serialized_memory() +
             self.edge_label_to_edge_id_serialized_memory() +
             self.edge_local_attr_keyvalue_mapping_serialized_memory() +
-            self.node_global_attr_keyvalue_mapping_serialized_memory() +
             self.node_local_attr_keyvalue_mapping_serialized_memory() +
             self.pdb_id_to_edges_serialized_memory() +
             self.pdb_code_to_id_serialized_memory() +
